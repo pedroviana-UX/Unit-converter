c6bd70da87991604d628e6aca9613a2d67d4149e
Pedro <pedrovianafei@gmail.com>
2026-07-22 09:36:55 +0000

    Realizado testes automatizados

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,1 +1,26 @@
 from src.converter import converter
+import pytest
+
+def test_metro_para_quilometro():
+    resultado = converter(1, "metro", "quilometro")
+    assert resultado == 0.001
+
+def test_quilometro_para_metro():
+    resultado = converter(1, "quilometro", "metro")
+    assert resultado == 1000
+
+def test_quilograma_para_grama():
+    resultado = converter(1, "quilograma", "grama")
+    assert resultado == 1000
+
+def test_celsius_para_fahrenheit():
+    resultado = converter(1, "celsius", "fahrenheit")
+    assert resultado == 33.8
+
+def test_unidades_incompativeis():
+    with pytest.raises(ValueError):
+        converter(10, "metro", "quilograma")
+
+def test_mesma_unidade():
+    resultado = converter(42, "metro", "metro")
+    assert resultado == 42      
