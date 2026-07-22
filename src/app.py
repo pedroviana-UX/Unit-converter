542575b396c0620aab6d375d3b046796a78cd5b7
Pedro <pedrovianafei@gmail.com>
2026-07-22 05:00:15 +0000

    adicionando funcionalidades no html e main.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from converter import converter
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/converter", methods = ["POST"])
+def rota_converter():
+    valor = float(request.form["valor"])
+    origem = request.form["unidade_origem"]
+    destino = request.form["unidade_destino"]
+
+    try:
+        resultado = converter(valor, origem, destino)
+        return render_template("index.html", resultado=round(resultado, 4), erro=None)
+    except ValueError as e:
+        return render_template("index.html", resultado=None, erro=str(e))
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ src/converter.py
@@ -0,0 +1,7 @@
+CATEGORIA_DA_UNIDADE = {
+    "metro": "comprimento", "centimetro": "comprimento", "quilometro": "comprimento",
+    "quilograma": "peso", "grama": "peso", "libra": "peso",
+    "celsius": "temperatura", "fahrenheit": "temperatura", "kelvin": "temperatura",
+}
+
+def converter_comprimento(valor, origem, destino):
